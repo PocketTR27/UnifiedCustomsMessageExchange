48e9dfdb7bd0e91d2415461dd81d3396615ad9c3
Matthieu Verbert <Matthieu.Verbert@iru.org>
2014-02-10 11:55:54 +0100

    Add 'test' artifacts to hold the keys

---
+++ plus/java/rtsplus-client-impl/src/test/java/org/iru/rtsplus/test/FileUtils.java
@@ -0,0 +1,17 @@
+package org.iru.rtsplus.test;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+class FileUtils {
+
+	static File locateFileInClasspath(String filename) {
+		URL uf = Thread.currentThread().getContextClassLoader().getResource(filename);
+		return new File(uf.getFile());
+	}
+	
+	public static byte[] loadFileInClasspath(String filename) throws IOException {
+		return org.apache.commons.io.FileUtils.readFileToByteArray(locateFileInClasspath(filename));
+	}
+}
