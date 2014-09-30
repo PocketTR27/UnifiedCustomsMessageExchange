fed412be4253f2d674eb588da45f63cf11c1ffcd
David Schenk <David.Schenk@iru.org>
2014-09-30 17:53:28 +0200

    Changes in RTSDotNETClient - fix the paths for certificates

--- classic/cs/RTSDotNETClient.TestClient/MainForm.cs
+++ classic/cs/RTSDotNETClient.TestClient/MainForm.cs
@@ -37,8 +37,8 @@ namespace RTSDotNETClient.TestClient
         {
             if (Program.LoadTestData)
             {
-                tbPfxFile.Text = Path.Combine(appDirectory, "RTSJAVA_recv.all.pfx");
-                tbCerFile.Text = Path.Combine(appDirectory, "RTSJAVA_send.cer.der");
+                tbPfxFile.Text = Path.Combine(appDirectory, Path.Combine("certificates", "RTSJAVA_recv.all.pfx"));
+                tbCerFile.Text = Path.Combine(appDirectory, Path.Combine("certificates", "RTSJAVA_send.cer.der"));
             }
         }
 
