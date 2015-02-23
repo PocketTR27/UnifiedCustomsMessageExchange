a2f94fad2686d8859e44aeb3d94a2432ea43027f
Matthieu Verbert <Matthieu.Verbert@iru.org>
2015-02-23 14:30:25 +0100

    Use ServiceLoader.load() instead of open-coded version(s)
    Thanks to Hugues Fauche for pointing out this "new" class.

--- plus/java/rtsplus-client-api/src/main/java/org/iru/rtsplus/client/ClientFactoryBuilder.java
+++ plus/java/rtsplus-client-api/src/main/java/org/iru/rtsplus/client/ClientFactoryBuilder.java
@@ -1,36 +1,18 @@
 package org.iru.rtsplus.client;
 
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.StringWriter;
+import java.util.Iterator;
+import java.util.ServiceLoader;
 
 public class ClientFactoryBuilder {
 
 	public static ClientFactory newInstance() {
-		ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
-		String fname = ClientFactory.class.getName();
-		InputStream is = contextClassLoader.getResourceAsStream("META-INF/services/"+fname);
-		if (is == null) {
-			throw new RuntimeException("No implementation found of "+fname);
-		}
-		try {
-		Reader r = new InputStreamReader(is, "UTF-8");
-		StringWriter w = new StringWriter();
-		int read;
-		while ((read = r.read()) != -1) {
-			w.write(read);
-		}
-		r.close();
-		
-		Class<ClientFactory> clazz = (Class<ClientFactory>) contextClassLoader.loadClass(w.toString());
-		
-		return clazz.newInstance();
-		} catch (Exception e) {
-			RuntimeException ie = new RuntimeException("Unable to instantiate a "+fname);
-			ie.initCause(e);
-			throw ie;
+		ServiceLoader<ClientFactory> serviceLoader = ServiceLoader.load(ClientFactory.class);
+		Iterator<ClientFactory> iter = serviceLoader.iterator();
+		if (iter.hasNext()) {
+			return iter.next();
+		} else {
+			throw new RuntimeException("No implementation found of "+ClientFactory.class.getName());
 		}
 	}
-	
+
 }
