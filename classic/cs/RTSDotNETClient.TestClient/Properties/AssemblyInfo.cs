56ea446961467f6fa6eb5d9e8a6d6a5e5285090d
David Schenk <David.Schenk@iru.org>
2016-03-08 12:25:06 +0100

    Release 2.4.3

--- classic/cs/RTSDotNETClient.TestClient/Properties/AssemblyInfo.cs
+++ classic/cs/RTSDotNETClient.TestClient/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.4.3.16068")]
-[assembly: AssemblyFileVersion("2.4.3.16068")]
+[assembly: AssemblyVersion("2.4.3.65534")]
+[assembly: AssemblyFileVersion("2.4.3.65534")]
