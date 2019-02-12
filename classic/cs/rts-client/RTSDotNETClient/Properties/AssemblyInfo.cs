2282aa780da3810a62dc6f3351e7908362d8327d
David Schenk <David.Schenk@iru.org>
2019-02-12 14:51:55 +0100

    Get ready for next development phase (2.14.0 [dev])

--- classic/cs/rts-client/RTSDotNETClient/Properties/AssemblyInfo.cs
+++ classic/cs/rts-client/RTSDotNETClient/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.13.1.18040")]
-[assembly: AssemblyFileVersion("2.13.1.18040")]
+[assembly: AssemblyVersion("2.14.0.19043")]
+[assembly: AssemblyFileVersion("2.14.0.19043")]
