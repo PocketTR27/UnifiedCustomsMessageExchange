0e6b83584f7d999c480ba73b735212ec8ad26abc
David Schenk <David.Schenk@iru.org>
2019-06-19 17:44:05 +0200

    Get ready for next development phase (2.15.1 [dev])

--- classic/cs/tirepd/IRU.Encryption/Properties/AssemblyInfo.cs
+++ classic/cs/tirepd/IRU.Encryption/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.15.0.65534")]
-[assembly: AssemblyFileVersion("2.15.0.65534")]
+[assembly: AssemblyVersion("2.15.1.19170")]
+[assembly: AssemblyFileVersion("2.15.1.19170")]
