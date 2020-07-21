e4cca00c109d5a2eddf8a8fad3dce92c6ccfa466
David Schenk <David.Schenk@iru.org>
2020-07-21 15:18:22 +0200

    Get ready for next development phase (2.16.0 [dev])

--- classic/cs/tirepd/B2GTester/Properties/AssemblyInfo.cs
+++ classic/cs/tirepd/B2GTester/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.15.3.20164")]
-[assembly: AssemblyFileVersion("2.15.3.20164")]
+[assembly: AssemblyVersion("2.16.0.20203")]
+[assembly: AssemblyFileVersion("2.16.0.20203")]
